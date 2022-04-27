8cbbababc53671e3b2da4fa76980b65721a7a54e
davide.boschiroli <davide.boschiroli@antaresvision.com>
2022-04-27 09:45:33 +0200

    First commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSendMessage = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSendMessage
+            // 
+            this.btnSendMessage.BackColor = System.Drawing.Color.Transparent;
+            this.btnSendMessage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSendMessage.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSendMessage.Location = new System.Drawing.Point(25, 29);
+            this.btnSendMessage.Name = "btnSendMessage";
+            this.btnSendMessage.Size = new System.Drawing.Size(299, 56);
+            this.btnSendMessage.TabIndex = 1;
+            this.btnSendMessage.Text = "Send Message To Go Server  (dot net client)";
+            this.btnSendMessage.UseVisualStyleBackColor = false;
+            this.btnSendMessage.Click += new System.EventHandler(this.btnSendMessage_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(25, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(299, 59);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Send Message To Go Server (GRPC Core)";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(359, 185);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnSendMessage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSendMessage;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Test.Communication.Grpc;
+using Google.Protobuf;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private static async Task AppendItemsRequestMethods()
+        {
+            List<AppendItemsRequest> grpcItems = new List<AppendItemsRequest>();
+            for (int i = 0; i < 200000; i++)
+            {
+                GrpcItem itm = new GrpcItem()
+                {
+                    Flags = "",
+                    HelperCode = "",
+                    Notification = 1,
+                    NtinId = 1422,
+                    PNtinId = null,
+                    PSerial = "",
+                    Sequence = i,
+                    Serial = "12345" + i,
+                    Status = 0,
+                    Type = 100,
+                };
+                grpcItems.Add(new AppendItemsRequest(){Item = itm});
+            }
+
+            var chanel = GrpcChannel.ForAddress("http://172.25.0.100:4424", new GrpcChannelOptions
+            {
+                //Credentials = ChannelCredentials.Insecure,
+                HttpHandler = new SocketsHttpHandler
+                {
+                    EnableMultipleHttp2Connections = true,
+                    PreAuthenticate = false,
+                    UseCookies = false,
+                    PooledConnectionIdleTimeout = Timeout.InfiniteTimeSpan,
+                },
+            });
+            var service = new GrpcService.GrpcServiceClient(chanel);
+            var call = service.AppendItems();
+            foreach (var r in grpcItems)
+            {
+                await call.RequestStream.WriteAsync(r).ConfigureAwait(false);
+            }
+            await call.RequestStream.CompleteAsync();
+        }
+
+        private static async Task AppendItemsRequestMethodsOLD()
+        {
+            List<AppendItemsRequest> grpcItems = new List<AppendItemsRequest>();
+            for (int i = 0; i < 200000; i++)
+            {
+                GrpcItem itm = new GrpcItem()
+                {
+                    Flags = "",
+                    HelperCode = "",
+                    Notification = 1,
+                    NtinId = 1422,
+                    PNtinId = null,
+                    PSerial = "",
+                    Sequence = i,
+                    Serial = "12345" + i,
+                    Status = 0,
+                    Type = 100,
+                };
+                grpcItems.Add(new AppendItemsRequest(){Item = itm});
+            }
+
+            var chanel = new Channel("172.25.0.100", 4424, ChannelCredentials.Insecure);
+            var service = new GrpcService.GrpcServiceClient(chanel);
+            var call = service.AppendItems();
+            foreach (var r in grpcItems)
+            {
+                await call.RequestStream.WriteAsync(r).ConfigureAwait(false);
+            }
+            await call.RequestStream.CompleteAsync();
+        }
+
+        private void btnSendMessage_Click(object sender, EventArgs e)
+        {
+            AppendItemsRequestMethods();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AppendItemsRequestMethodsOLD();
+        }
+    }
+}
